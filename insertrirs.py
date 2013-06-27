23c12de427d19e61a299728e22f0f2e70301ac5a
Remy de Boer <remydb@gmail.com>
2013-06-27 12:00:34 +0200

    Removed testing mysql credentials

--- insertrirs.py
+++ insertrirs.py
@@ -2,18 +2,33 @@
 
 import MySQLdb as mdb
 from time import strftime
+import ipaddr
 
-source = open("rirs", "r")
-
-connection=mdb.connect('localhost', 'root', 'noedelsoep', 'bgp') #Don't worry, not a real password
+connection=mdb.connect('localhost', 'username', 'password', 'bgp') #Don't worry, not a real password
 cursor=connection.cursor()
 
 tablepart = strftime("%d-%m-%y")
+
+#IPv4:
+source = open("rirs", "r")
 
 for line in source:
 	prefix = line.rstrip().split(" ")[0]
 	rir = line.rstrip().split(" ")[1]
 	cursor.execute("UPDATE `%s_routes` SET `RIR` = '%s' WHERE IPver = '4' AND `Prefix` LIKE '%s.%%'" % (tablepart, rir, prefix))
+
+connection.commit()
+
+#IPv6:
+source = open("rirs-ipv6", "r")
+
+for line in source:
+	prefix = line.rstrip().split(" ")[0]
+	binprefix = "%0128d\n" % (long(bin(ipaddr.IPv6Network(prefix).network)[2:]))
+	length = int(prefix.split("/")[1])
+	matchprefix = "%s" % (binprefix[:length])
+	rir = line.rstrip().split(" ")[1]
+	cursor.execute("UPDATE `%s_routes` SET `RIR` = '%s' WHERE IPver = '6' AND `Bin` LIKE '%s%%'" % (tablepart, rir, matchprefix))
 
 connection.commit()
 connection.close()
