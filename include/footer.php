792c349b9be5dd147ff9684d459624ab52fa9cc6
remydb <remydb@gmail.com>
2013-07-05 17:36:32 +0200

    Update footer.php
    Added links to footer

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
   <div class='row-fluid'>
     <div class='span3'>
-      <p>By Remy de Boer & Javy de Koning</p>
+      <p>By <a href="https://github.com/remydb/">Remy de Boer</a> & <a href="http://www.linkedin.com/profile/view?id=66902155">Javy de Koning</a></p>
     </div>
     <div class='span4 offset5'>
       <p>Page generated on: <?php echo date(DATE_RFC822);?></p>
